449a5762ceb0b93da22c08c4de268041fa4e203b
tdeporte <tom.deporte@etu.u-bordeaux.fr>
2022-02-07 03:19:48 +0100

    minor changes to the scene

--- Assets/Script/Arduino.cs
+++ Assets/Script/Arduino.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Arduino : MonoBehaviour
 {
-    public float magnetForce = 2;
+    public float magnetForce = 10;
     public bool docked = false;
+    public float TriggerDistance = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +17,16 @@ public class Arduino : MonoBehaviour
     void FixedUpdate()
     {
         RaycastHit hit;
-        float TriggerDistance = 2;
+        
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, TriggerDistance))
         {
             Vector3 direction = hit.point - transform.position;
             direction.Normalize ();
 
-            gameObject.GetComponent<Rigidbody>().AddForceAtPosition(magnetForce * direction, hit.point);
+            //gameObject.GetComponent<Rigidbody>().AddForceAtPosition(magnetForce * direction, hit.point);
+
+            gameObject.GetComponent<Rigidbody>().velocity = (hit.transform.position - (gameObject.GetComponent<Rigidbody>().transform.position + gameObject.GetComponent<Rigidbody>().centerOfMass)) * magnetForce * Time.deltaTime;
 
             Debug.DrawRay(transform.position, transform.TransformDirection(direction) * hit.distance, Color.green);
 
@@ -34,10 +37,11 @@ public class Arduino : MonoBehaviour
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 20, Color.red);
+
+            docked = false;
             
             //Debug.Log("Did not Hit");
 
-            //docked = false;
         }
         
     }
